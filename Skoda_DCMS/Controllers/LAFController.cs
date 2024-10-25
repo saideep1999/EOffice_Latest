d6009d1cc09014bd0281f3fd83064257806e9982
Saideep <saideep.s@mobinexttech.com>
2024-10-25 19:31:53 +0530

    code251020240731

---
+++ Skoda_DCMS/Controllers/LAFController.cs
@@ -0,0 +1,29 @@
+﻿using Skoda_DCMS.DAL;
+using Skoda_DCMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Skoda_DCMS.Controllers
+{
+    public class LAFController : BaseController
+    {
+        // GET: LAF
+        LAFDAL lafDAL;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> SaveLAF(FormCollection form)
+        {
+
+            lafDAL = new LAFDAL();
+            var result = await lafDAL.SaveLAF(form, (UserData)Session["UserData"]);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Skoda_DCMS/Models/LAFModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Skoda_DCMS.Models
+{
+    public class LAFModel : ApplicantDataModel
+    {
+        public int Id { get; set; }
+        public DateTime Created_Date { get; set; }
+
+        [Required]
+        [Display(Name = "Employee ID")]
+        public string LAFEmployeeID { get; set; }
+
+        [Required]
+        [Display(Name = "Leave Type")]
+        public string LeaveType { get; set; }
+
+        [Required]
+        [Display(Name = "Leave Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime? LeaveStartDate { get; set; }
+
+        [Required]
+        [Display(Name = "Leave End Date")]
+        [DataType(DataType.Date)]
+        public DateTime? LeaveEndDate { get; set; }
+
+        [Display(Name = "Total Leave Days")]
+        public int TotalLeaveDays { get; set; }
+        
+        [Required]
+        [Display(Name = "Reason for Leave")]
+        [DataType(DataType.MultilineText)]
+        public string LeaveReason { get; set; }
+    }
+}
